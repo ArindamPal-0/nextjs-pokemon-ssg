6b53783cf028662a3c7419f5db2dad7e7b971397
Arindam Pal <arindampal2000@gmail.com>
2022-08-11 00:23:00 +0530

    adding pokemon list code with SSG

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,97 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/Details.module.css";
+
+interface Stat {
+  name: string;
+  value: number;
+}
+
+interface PokemonDetails {
+  name: string;
+  type: Array<String>;
+  stats: Array<Stat>;
+  image: string;
+}
+
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface Props {
+  pokemon: PokemonDetails;
+}
+
+export async function getStaticPaths() {
+  const resp = await fetch(
+    `https://arindampal-0.github.io/pokemon-api/index.json`
+  );
+
+  const pokemons = (await resp.json()) as Array<Pokemon>;
+
+  return {
+    paths: pokemons.map((pokemon) => ({
+      params: { id: pokemon.id.toString() },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { id: number } }) {
+  const resp = await fetch(
+    `https://arindampal-0.github.io/pokemon-api/pokemon/${params.id}.json`
+  );
+
+  return {
+    props: {
+      pokemon: (await resp.json()) as PokemonDetails,
+    },
+  };
+}
+
+export default function Details({ pokemon }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{pokemon?.name}</title>
+      </Head>
+      <div>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+        <div className={styles.layout}>
+          <div>
+            <img
+              className={styles.picture}
+              src={`https://arindampal-0.github.io/pokemon-api/${pokemon?.image}`}
+              alt={pokemon?.name}
+            />
+          </div>
+          <div>
+            <div className={styles.name}>{pokemon?.name}</div>
+            <div className={styles.type}>{pokemon?.type.join(", ")}</div>
+            <table>
+              <thead className={styles.header}>
+                <tr>
+                  <th>Name</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pokemon?.stats.map(({ name, value }: Stat) => (
+                  <tr key={name}>
+                    <td className={styles.attribute}>{name}</td>
+                    <td>{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
